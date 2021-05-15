8e614ee2e719c4a649ecedc84f4eec9c60484299
Artur <artur300998@gmail.com>
2021-05-16 00:36:29 +0200

    Added multi-threaded test case for the GSIServer that uses std::future.

--- test/GSIServerTest.cpp
+++ test/GSIServerTest.cpp
@@ -3,17 +3,30 @@
 #include <fstream>
 #include <iostream>
 #include <string>
+#include <future>
 
 #include "../src/GSIServer.h"
 #include "gtest/gtest.h"
+
+
 
 class GSIServerTestFixture : public ::testing::Test
 {
 protected:
 	void SetUp() override
 	{
+		const std::string filename = "../test/post_body_sample.txt";
+		std::ifstream ifs(filename);
+		if(!ifs.good())
+			FAIL() << "Couldn't load POST body sample from " + filename;
+		std::stringstream ss;
+		ss << ifs.rdbuf();
+		postBodySample = ss.str();
+
 		client = std::make_unique<httplib::Client>("127.0.0.1", 3000);
 		server = std::make_unique<GSIServer>("127.0.0.1", 3000);
+
+
 	}
 
 	void TearDown() override
@@ -25,7 +38,7 @@ protected:
 	{
 		httplib::Headers headers = {
 				{"user-agent", "Valve/Steam HTTP Client 1.0 (730)"},
-				{"Accept", "text/html*/*;q=0.9"},
+				{"accept", "text/html*/*;q=0.9"},
 				{"accept-encoding", "gzip,identity,*;q=0"},
 				{"accept-charset", "ISO-8859-1,utf-8,*;q=0.7"}
 		};
@@ -35,16 +48,34 @@ protected:
 
 	std::unique_ptr<httplib::Client> client;
 	std::unique_ptr<GSIServer> server;
+	std::string postBodySample;
 };
 
-TEST_F(GSIServerTestFixture, TestCase1)
+TEST_F(GSIServerTestFixture, ShouldTransferBodySampleFromClientToServer)
 {
-	std::ifstream ifs("../test/post_body_sample.txt");
-	std::stringstream ss;
-	ss << ifs.rdbuf();
-	std::string fileContent = ss.str();
+	sendPostRequest(postBodySample);
 
-	sendPostRequest(fileContent);
-
-	ASSERT_STREQ(fileContent.c_str(), server->getNextDataOrWait().c_str());
+	ASSERT_STREQ(postBodySample.c_str(), server->getNextDataOrWait().c_str());
 }
+
+TEST_F(GSIServerTestFixture, ShouldWaitUntilNewDataComes)
+{
+	using namespace std::chrono_literals;
+
+	std::future<std::string> f = std::async(std::launch::async, [&]{
+		return server->getNextDataOrWait();
+	});
+
+	if(auto result = sendPostRequest(postBodySample))
+	{
+		std::future_status futureStatus = f.wait_for(1s);
+		if (futureStatus != std::future_status::ready)
+			FAIL() << "The server did not return the data.";
+
+		if(result->status == 200)
+			SUCCEED();
+		else
+			FAIL() << "Response from server was not '200'. 'Error' enum value is '" + std::to_string(result.error()) + "'";
+	}
+}
+
--- test/test.cpp
+++
@@ -1,7 +0,0 @@
-#include "gtest/gtest.h"
-
-TEST(HelloTest, BasicAssertions)
-{
-	EXPECT_STREQ("hello", "world") << "Strings are not equal";
-	EXPECT_EQ(7*6,42);
-}
